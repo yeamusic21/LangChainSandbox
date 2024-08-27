d66e9828ebf2ac5bfb3afbc66445d399fb5a43f1
Matt <yeamusic21@yahoo.com>
2024-08-26 23:09:44 -0400

    agent deepdive completed

--- react_langchain_deepdive.py
+++ react_langchain_deepdive.py
@@ -7,6 +7,7 @@ from langchain_ollama import ChatOllama
 from langchain.agents.output_parsers.react_single_input import ReActSingleInputOutputParser
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain.agents.format_scratchpad.log import format_log_to_str
+from react_langchain_callbacks import AgentCallbackHandler
 
 @tool
 def get_text_length(text:str) -> int:
@@ -48,40 +49,65 @@ if __name__ == "__main__":
         tools=render_text_description(tools), 
         tool_names=", ".join([t.name for t in tools])
     )
-    llm = ChatOllama(model="gemma2", stop=["\nObservation"])
+    llm = ChatOllama(model="gemma2", stop=["\nObservation"], callbacks=[AgentCallbackHandler()])
     intermediate_steps = []
     agent = {"input": lambda x:x["input"], "agent_scratchpad": lambda x:format_log_to_str(x['agent_scratchpad'])} | prompt | llm | ReActSingleInputOutputParser()
-    print("------------- CALL 1")
-    print("Note that the first call only determines what needs to be done, and only afterwards do we apply a tool...")
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-        {
-            "input":"What is the length in characters of the text DOG?",
-            "agent_scratchpad": intermediate_steps
-        }
-    )
-    print("agent_step: ", agent_step)
-    print("agent_step type: ", type(agent_step))
-    if isinstance(agent_step, AgentAction):
-        tool_name = agent_step.tool
-        tool_to_use = find_tool_by_name(tools, tool_name)
-        tool_input = agent_step.tool_input
-        observation = tool_to_use.func(str(tool_input))
-        print("observation: ", f"{observation}")
-        intermediate_steps.append((agent_step, str(observation)))
-    print("------------- CALL 2")
-    print("Note that previously we had a first crack at 1) what tool to apply 2) applying that tool. ")
-    print("Now we're going to make a 2nd call where it's possible we obtained the answer after applying the 1st tool.")
-    print("If not, then we need to determine next steps ...")
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-        {
-            "input":"What is the length in characters of the text DOG?",
-            "agent_scratchpad": intermediate_steps
-        }
-    )
-    print("agent_step: ", agent_step)
-    print("agent_step type: ", type(agent_step))
+    # start while loop
+    agent_step=""
+    while not isinstance(agent_step,AgentFinish):
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {
+                "input":"What is the length in characters of the text DOG?",
+                "agent_scratchpad": intermediate_steps
+            }
+        )
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input
+            observation = tool_to_use.func(str(tool_input))
+            print("agent_step: ", agent_step)
+            print("observation: ", f"{observation}")
+            intermediate_steps.append((agent_step, str(observation)))
     if isinstance(agent_step, AgentFinish):
         print("Final Answer: ", agent_step.return_values)
+
+
+
+
+
+
+    # print("------------- CALL 1")
+    # print("Note that the first call only determines what needs to be done, and only afterwards do we apply a tool...")
+    # agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+    #     {
+    #         "input":"What is the length in characters of the text DOG?",
+    #         "agent_scratchpad": intermediate_steps
+    #     }
+    # )
+    # print("agent_step: ", agent_step)
+    # print("agent_step type: ", type(agent_step))
+    # if isinstance(agent_step, AgentAction):
+    #     tool_name = agent_step.tool
+    #     tool_to_use = find_tool_by_name(tools, tool_name)
+    #     tool_input = agent_step.tool_input
+    #     observation = tool_to_use.func(str(tool_input))
+    #     print("observation: ", f"{observation}")
+    #     intermediate_steps.append((agent_step, str(observation)))
+    # print("------------- CALL 2")
+    # print("Note that previously we had a first crack at 1) what tool to apply 2) applying that tool. ")
+    # print("Now we're going to make a 2nd call where it's possible we obtained the answer after applying the 1st tool.")
+    # print("If not, then we need to determine next steps ...")
+    # agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+    #     {
+    #         "input":"What is the length in characters of the text DOG?",
+    #         "agent_scratchpad": intermediate_steps
+    #     }
+    # )
+    # print("agent_step: ", agent_step)
+    # print("agent_step type: ", type(agent_step))
+    # if isinstance(agent_step, AgentFinish):
+    #     print("Final Answer: ", agent_step.return_values)
 
 
 
